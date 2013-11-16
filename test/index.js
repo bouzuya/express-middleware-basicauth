666cec89f7a07fd14540a675ebe512dd27689bd4
bouzuya <bouzuya@gmail.com>
2013-11-16 14:40:07 +0900

    added basicAuth(callback) examples.

--- test/index.js
+++ test/index.js
@@ -93,6 +93,88 @@ describe('basicAuth middleware tests', function() {
     });
   });
 
+  describe('app.get("/", express.basicAuth(function(u,p)...), ...)', function() {
+    var app = express();
+    var auth = express.basicAuth(function(user, pass) {
+      return user === USERNAME && pass === PASSWORD;
+    });
+    app.get('/', auth, function(req, res) { res.send('Root'); });
+    app.get('/hello', function(req, res) { res.send('Hello'); });
+
+    describe('without auth', function() {
+      describe('GET /', function() {
+        it('should return 401', function(done) {
+          supertest(app).get('/').expect(401, done);
+        });
+      });
+
+      describe('GET /hello', function() {
+        it('should return 200 "Hello"', function(done) {
+          supertest(app).get('/hello').expect(200).expect('Hello', done);
+        });
+      });
+    });
+
+    describe('with auth', function() {
+      describe('GET /', function() {
+        it('should return 200 "Root"', function(done) {
+          supertest(app).get('/').auth(USERNAME, PASSWORD).expect(200).expect('Root', done);
+        });
+      });
+      describe('GET /hello', function() {
+        it('should return 200 "Hello"', function(done) {
+          supertest(app).get('/hello').auth(USERNAME, PASSWORD).expect(200).expect('Hello', done);
+        });
+      });
+    });
+  });
+
+  describe('app.get("/", express.basicAuth(function(u,p,c)...), ...)', function() {
+    var app = express();
+    var auth = express.basicAuth(function(user, pass, callback) {
+      // callback for async
+      var tasks = [];
+      tasks.push(function(next) { next(); });
+      tasks.push(function(next) { next(); });
+      async.waterfall(tasks, function(err, result) {
+        if (user === USERNAME && pass === PASSWORD) {
+          callback(null, { user: user, some: 'thing', banana: 64 });
+        } else {
+          callback();
+        }
+      });
+    });
+    app.get('/', auth, function(req, res) { res.send('Root'); });
+    app.get('/hello', function(req, res) { res.send('Hello'); });
+
+    describe('without auth', function() {
+      describe('GET /', function() {
+        it('should return 401', function(done) {
+          supertest(app).get('/').expect(401, done);
+        });
+      });
+
+      describe('GET /hello', function() {
+        it('should return 200 "Hello"', function(done) {
+          supertest(app).get('/hello').expect(200).expect('Hello', done);
+        });
+      });
+    });
+
+    describe('with auth', function() {
+      describe('GET /', function() {
+        it('should return 200 "Root"', function(done) {
+          supertest(app).get('/').auth(USERNAME, PASSWORD).expect(200).expect('Root', done);
+        });
+      });
+      describe('GET /hello', function() {
+        it('should return 200 "Hello"', function(done) {
+          supertest(app).get('/hello').auth(USERNAME, PASSWORD).expect(200).expect('Hello', done);
+        });
+      });
+    });
+  });
+
 });
 
 
